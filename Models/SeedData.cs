e25cb07770a7a4640b6ed732e9c1f31ace53c2f7
Jump3rr <theneonxd@gmail.com>
2021-11-15 02:32:06 +0100

    Added SeedData, new MembershipTypes and creating HostBuilder in Main

---
+++ Models/SeedData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibApp.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LibApp.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+                if (context.MembershipTypes.Any())
+                {
+                    Console.WriteLine("Database already seeded");
+                    return;
+                }
+                context.MembershipTypes.AddRange(
+                    new MembershipType
+                    {
+                        Id =1,
+                        SignUpFee = 0,
+                        DurationInMonth = 0,
+                        DiscountRate = 0    
+                    },
+                    new MembershipType
+                    {
+                        Id = 2,
+                        SignUpFee = 30,
+                        DurationInMonth = 1,
+                        DiscountRate = 10
+                    },
+                    new MembershipType
+                    {
+                        Id = 3,
+                        SignUpFee = 90,
+                        DurationInMonth = 3,
+                        DiscountRate = 15
+                    },
+                    new MembershipType
+                    {
+                        Id = 4,
+                        SignUpFee = 300,
+                        DurationInMonth = 12,
+                        DiscountRate = 20
+                    });
+                context.SaveChanges();
+            }
+        }
+    }
+}
