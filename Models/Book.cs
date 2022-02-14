be45fb2b60d6d2684a83cc0fe7780bf0293c25bc
PATRYK KAMUSINSKI <theneonxd@gmail.com>
2022-02-14 21:56:15 +0100

    task 2

--- Models/Book.cs
+++ Models/Book.cs
@@ -9,17 +9,21 @@ namespace LibApp.Models
     public class Book
     {
         public int Id { get; set; }
-		[Required]
+		[Required(ErrorMessage = "Name is required.")]
 		[StringLength(255)]
 		public string Name { get; set; }
-		[Required]
+		[Required(ErrorMessage = "Author name is required.")]
 		public string AuthorName { get; set; }
-		[Required]
+		[Required(ErrorMessage = "Genre is required.")]
 		public Genre Genre { get; set; }
 		public byte GenreId { get; set; }
 		public DateTime DateAdded { get; set; }
+		[Required(ErrorMessage = "Release date is required.")]
 		public DateTime ReleaseDate { get; set; }
+		[Required(ErrorMessage = "Number in stock is required.")]
+		[Range(1, 20, ErrorMessage = "Number must be between 1 and 20.")]
 		public int NumberInStock { get; set; }
+		[Required(ErrorMessage = "Number available is required.")]
 		public int NumberAvailable { get; set; }
 	}
       
