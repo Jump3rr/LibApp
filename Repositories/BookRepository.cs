43e9eef61de533c1c0312ac2b86e49ba30e6d027
PATRYK KAMUSINSKI <theneonxd@gmail.com>
2022-02-15 21:46:28 +0100

    task 1

---
+++ Repositories/BookRepository.cs
@@ -0,0 +1,29 @@
+﻿using LibApp.Data;
+using LibApp.Interfaces;
+using LibApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibApp.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public BookRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Book> GetBooks()
+        {
+            return _context.Books;
+        }
+
+        public void AddBook(Book book) => _context.Books.Add(book);
+        public Book GetBookById(int bookId) => _context.Books.Find(bookId);
+        public void DeleteBook(int bookId) => _context.Books.Remove(GetBookById(bookId));
+        public void UpdateBook(Book book) => _context.Books.Update(book);
+        public void Save() => _context.SaveChanges();
+    }
+}
